f272b0d9da60336cbd0b0111eab436da64cba89f
sivatejpopuri <145978321+sivatejpopuri@users.noreply.github.com>
2023-09-26 15:46:23 +1000

    updating
    updating the code

--- inheritance.js
+++ inheritance.js
@@ -10,10 +10,21 @@ class Student extends Person{
     {
         return 'QUT'
     }
+    marks()
+    {
+        if(marks > 20)
+        {
+            console.log('true',marks)
+        }
+        else{
+            console.log('false')
+        }
+        return marks
+    }
 
 
 }
-let student1 = new Student('John', 'Park', 18, 'M', 999)
+let student1 = new Student('John', 'Park', 18, 'M', 24)
 console.log(student1)
 
 console.log(student1.location())
@@ -21,7 +32,7 @@ console.log(student1.location())
 // hi
 
 console.log(student1.fullName())
-
+console.log(student1.marks())
 
 
 
